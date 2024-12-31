da28e62d2f2afe2b9b6b8eeb0790d28b8c9dead4
Admin <Admin@DESKTOP-6R75LHS>
2024-12-31 20:28:59 +0500

    Add project files.

---
+++ Intro to web project mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Intro_to_web_project_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
